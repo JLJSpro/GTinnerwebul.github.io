48495cf537c83a5c5cd8bed76a63f86bdd10a9bd
JLJSpro <166625634+JLJSpro@users.noreply.github.com>
2024-06-13 19:15:47 +0800

    Add files via upload

---
+++ eduweb/que.js
@@ -0,0 +1,8 @@
+let button = document.body.getElementById("Q1A")
+let ans = document.body.getElementById("ans1")
+
+let press = function(){
+    ans.color = 'black'
+}
+
+button.addEventListener('click', press)
